2618a906d52dd413fa66ffd68227b772a8e398d3
Yoshi Yamaguchi <yoshifumi@google.com>
2022-06-27 17:01:15 +0900

    fix: use hypothesis.event as the counterpart of collect/2

--- CustomGenerators/test_generators.py
+++ CustomGenerators/test_generators.py
@@ -12,7 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from hypothesis import given
+from collections import defaultdict
+
+from hypothesis import event, given
 from hypothesis.strategies import (booleans, characters, composite, integers,
                                    lists, one_of, sampled_from, text, tuples)
 
@@ -26,9 +28,31 @@ def keys(draw):
 def vals(draw):
     return draw(booleans() | characters() | integers() | text())
 
-
+# In order to print the statistics from `event`, you need to specify
+# Hypothesis' statistics option: `--hypothesis-show-statistics`.
+# See details here: https://hypothesis.readthedocs.io/en/latest/details.html#statistics
 @given(lists(tuples(keys(), vals())))
 def test_dupes(kv):
     m = {k: v for k, v in kv}
     [m[k] for k, _ in kv] # I want non existing key to be crashed
+    range_min, range_max = to_range(5, len(kv) - len(ukey(kv)))
+    event(f"dupes: {range_min}-{range_max}")
 
+
+def to_range(m, n):
+    base = n // m
+    return (base*m, (base+1)*m)
+
+
+def ukey(lt):
+    """ukeysort sorts list of tuples based on nth element in tuple and removed
+    duplicated elements from it.
+
+    Args:
+        lt (List[Tuple])): List of tuples
+    """
+    ret = defaultdict(list)
+    for e in lt:
+        ret[e[0]].append(e[1])
+    ret = dict(ret)
+    return ret
